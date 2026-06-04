d79a47a5361b90947b3496e463cd73a300621d3b
Vini-Lino <vinilinoferreira@proton.me>
2026-06-04 20:52:10 -0300

    Start structure

---
+++ views/menu_view.py
@@ -0,0 +1,11 @@
+#a parte visual inicial das opções do cadastro, coloca 4 opções, sendo elas:
+#logar
+#cadastrar
+#listar funcionários
+#buscar funcionários
+#sair
+
+#é só uma função def com os prints de forma visual, nada muito complexo
+
+def exibir_menu():
+    pass #a função do menu
