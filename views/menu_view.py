48e66458f4dca64f2d4e168b90bc62728f32547b
Vini-Lino <vinilinoferreira@proton.me>
2026-06-04 14:14:56 -0300

    Adicionando o view

---
+++ views/menu_view.py
@@ -0,0 +1,7 @@
+def exibir_menu():
+
+    print("\nBem Vindo ao Solais")
+    print("Para entrar no sistema, primeiro entre com uma conta: ")
+    print("1 Login")
+    print("2 Cadastrar")
+    print("3 Sair")
